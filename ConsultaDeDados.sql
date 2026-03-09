528b0f95ff9c5a85319d21a6187fa61aed9584db
leomacedo <leonardo.aurieni@aluno.ifsp.edu.br>
2026-03-08 22:07:43 -0300

    Add files via upload

---
+++ ConsultaDeDados.sql
@@ -0,0 +1,43 @@
+-- Tabela classroom
+SELECT COUNT(*) AS total_registros_classroom FROM classroom;
+-- Total registros: 30
+
+-- Tabela department
+SELECT COUNT(*) AS total_registros_department FROM department;
+-- Total registros: 20
+
+-- Tabela course
+SELECT COUNT(*) AS total_registros_course FROM course;
+-- Total registros: 200
+
+-- Tabela instructor
+SELECT COUNT(*) AS total_registros_instructor FROM instructor;
+-- Total registros: 50
+
+-- Tabela section
+SELECT COUNT(*) AS total_registros_section FROM section;
+-- Total registros: 100
+
+-- Tabela teaches
+SELECT COUNT(*) AS total_registros_teaches FROM teaches;
+-- Total registros: 100
+
+-- Tabela student
+SELECT COUNT(*) AS total_registros_student FROM student;
+-- Total registros: 2000
+
+-- Tabela takes
+SELECT COUNT(*) AS total_registros_takes FROM takes;
+-- Total registros: 30000
+
+-- Tabela advisor
+SELECT COUNT(*) AS total_registros_advisor FROM advisor;
+-- Total registros: 2000
+
+-- Tabela time_slot
+SELECT COUNT(*) AS total_registros_time_slot FROM time_slot;
+-- Total registros: 20
+
+-- Tabela prereq
+SELECT COUNT(*) AS total_registros_prereq FROM prereq;
+-- Total registros: 100
